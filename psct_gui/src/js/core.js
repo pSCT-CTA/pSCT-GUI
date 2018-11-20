7a0051437112e925b1b2db4a8c5ef6771c00c8fa
bryankim96 <bryan.sanghyuk.kim@gmail.com>
2018-11-20 12:51:37 -0800

    Updates to python package imports and config files.

--- psct_gui/src/js/core.js
+++ psct_gui/src/js/core.js
@@ -1,5 +1,8 @@
 import * as $ from 'jquery';
 
+import cta_logo from '../img/cta_logo.png';
+import psct_logo from '../img/cta_psct_logo.png';
+
 // Function to enable switching between tabs
 export function enableTabs() {
 
@@ -8,27 +11,6 @@ export function enableTabs() {
             $(this).tab('show');
         }
     );
-}
-
-// Function to switch between tabs
-export function showLoginAlert(login_attempt) {
-    var $this = $("#login_alert");
-    $this.removeClass();
-    $this.addClass("alert alert-dismissible fade");
-
-    if (login_attempt == 'fail') {
-        $this.children('.alert-text').text('Login failed. Incorrect username or password.');
-        $this.addClass("alert-danger");
-    }
-    else if (login_attempt == 'redirect') {
-        $this.children('.alert-text').text('Sorry, please log in first.');
-        $this.addClass("alert-warning");
-    }
-    else if (login_attempt == 'logout') {
-        $this.children('.alert-text').text('Successfully logged out.');
-        $this.addClass("alert-success");
-    }
-    $this.show();
 }
 
 // Function to resize card on click
