bf2852b5bf99b66470ea3c30031c0c1c6c22b0b5
bryankim96 <bryan.sanghyuk.kim@gmail.com>
2019-02-15 10:39:30 -0700

    Changed data request to use two socketio messages rather than callbacks.
    Added corresponding new_data handler.

--- psct_gui/src/socketio-device-client.js
+++ psct_gui/src/socketio-device-client.js
@@ -1,78 +1,78 @@
 export class BaseSocketioDeviceClient {
-  constructor(address, component) {
+  constructor (address, component) {
     this.socket = io(address)
     this.component = component
 
     // General-purpose event handlers
-    this.socket.on('connect', function(){
+    this.socket.on('connect', function () {
       console.log(`[socket.io] - Connected - ${this.component.name}, id=${this.socket.id}.`)
     }.bind(this))
-    this.socket.on('disconnect', function(){
+    this.socket.on('disconnect', function () {
       console.log(`[socket.io] - Disconnected - ${this.component.name}, id=${this.socket.id}.`)
     }.bind(this))
-    this.socket.on('connect_error', function(){
+    this.socket.on('connect_error', function () {
       console.log(`[socket.io] - Connect error - ${this.component.name}, id=${this.socket.id}.`)
     }.bind(this))
-    this.socket.on('connect_timeout', function(){
+    this.socket.on('connect_timeout', function () {
       console.log(`[socket.io] - Connect timeout - ${this.component.name}, id=${this.socket.id}.`)
     }.bind(this))
-    this.socket.on('error', function(){
+    this.socket.on('error', function () {
       console.log(`[socket.io] - Error - ${this.component.name}, id=${this.socket.id}.`)
     }.bind(this))
-    this.socket.on('reconnect', function(){
+    this.socket.on('reconnect', function () {
       console.log(`[socket.io] - Reconnected - ${this.component.name}, id=${this.socket.id}.`)
     }.bind(this))
-    this.socket.on('reconnect_attempt', function(){
+    this.socket.on('reconnect_attempt', function () {
       console.log(`[socket.io] - Reconnect attempt - ${this.component.name}, id=${this.socket.id}.`)
     }.bind(this))
-    this.socket.on('reconnecting', function(){
+    this.socket.on('reconnecting', function () {
       console.log(`[socket.io] - Reconnect attempt - ${this.component.name}, id=${this.socket.id}.`)
     }.bind(this))
-    this.socket.on('reconnect_error', function(){
+    this.socket.on('reconnect_error', function () {
       console.log(`[socket.io] - Reconnect error - ${this.component.name}, id=${this.socket.id}.`)
     }.bind(this))
-    this.socket.on('reconnect_failed', function(){
+    this.socket.on('reconnect_failed', function () {
       console.log(`[socket.io] - Reconnect failed - ${this.component.name}, id=${this.socket.id}.`)
     }.bind(this))
 
-    // Device client-specific handlers
-    this.socket.on('data_change', this.on_data_change.bind(this))
-    this.socket.on('method_return', this.on_method_return.bind(this))
-    this.socket.on('method_stopped', this.on_method_stopped.bind(this))
-    this.socket.on('device_busy', this.on_device_busy.bind(this))
+    // Device-specific handlers
+    this.socket.on('new_data', this._onNewData.bind(this))
+    this.socket.on('data_change', this._onDataChange.bind(this))
+    this.socket.on('method_return', this._onMethodReturn.bind(this))
+    this.socket.on('method_stopped', this._onMethodStopped.bind(this))
+    this.socket.on('device_busy', this._onDeviceBusy.bind(this))
   }
 
-  on_data_change(data) {
+  _onNewData (data) {
   }
-  on_method_return(data) {
+  _onDataChange (data) {
   }
-  on_method_stopped(data) {
+  _onMethodReturn (data) {
   }
-  on_device_busy(data) {
+  _onMethodStopped (data) {
+  }
+  _onDeviceBusy (data) {
   }
 
-  request_all_data(devices_by, selection=null) {
-    var requestData = {
+  requestData (devicesBy, selection = null) {
+    var request = {
       component_name: this.component.name,
-      devices_by: devices_by}
-    if (devices_by === "types") {
-      requestData.types = selection
+      devices_by: devicesBy }
+    if (devicesBy === 'types') {
+      request.types = selection
+    } else if (devicesBy === 'ids') {
+      request.ids = selection
+    } else if (devicesBy === 'all') {
+    } else {
+      throw new Error('Invalid devices_by mode.')
     }
-    else if (devices_by === "ids") {
-      requestData.ids = selection
-    }
-    else if (devices_by === "all") {
-    }
-    else {
-      throw new Error("Invalid devices_by mode.")
-    }
-    this.socket.emit('request_all_data', requestData, this.component.setAllData.bind(this.component))
+    this.socket.emit('request_data', request)
   }
 
-  connect() {
+  connect () {
     this.socket.connect()
   }
-  disconnect() {
+  disconnect () {
     this.socket.disconnect()
   }
 }
